23c0722bfc712e247558000cbcd15cf2c9b88e0e
Anthony Castillo <anthony1093ca@gmail.com>
2024-09-26 02:02:23 -0700

    Added type hints for functions & error check users

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ from dice import Dice
 # https://twitchio.dev/en/stable/exts/routines.html
 
 class Bot(commands.Bot):
-    def __init__(self):
+    def __init__(self) -> None:
         self.__currTime = time.localtime()
         if not Path('./logs').is_dir():
             Path('./logs').mkdir()
@@ -40,7 +40,7 @@ class Bot(commands.Bot):
                 self.__command_names.append(cmd)
     # end __init__
 
-    def __del__(self):
+    def __del__(self) -> None:
         self._printLog('Shutting down bot...')
         if isinstance(self.__currTime, bool):
             self.__currTime
@@ -50,7 +50,7 @@ class Bot(commands.Bot):
         self._printLog('-----------------------------------------------------------')
     # end __del__
 
-    def _configureFilename(self, timeData):
+    def _configureFilename(self, timeData) -> str:
         currTimeString = str(timeData[0])
         if len(str(timeData[1])) == 1:
             currTimeString += '0'+str(timeData[1])
@@ -63,13 +63,13 @@ class Bot(commands.Bot):
         return currTimeString
     # end configureFilename
     
-    def _printLog(self, text:str):
+    def _printLog(self, text:str)  -> None:
         currTime = str(time.ctime())
         print(currTime+' - '+text)
         logging.info(currTime+' - '+text)
     # end _printLog
 
-    def _checkFiles(self):
+    def _checkFiles(self) -> bool:
         status = []
         if not Path('./keys').is_dir():
             Path('./keys').mkdir()
@@ -110,14 +110,14 @@ class Bot(commands.Bot):
             return True
     # end _checkDirs
 
-    def _readToken(self):
+    def _readToken(self) -> str:
         with open('./keys/token.txt', 'r') as file:
             for line in file.readlines():
                 if line != '' and len(line) > 0:
                     return line        
     # end _readToken
 
-    def _readChannel(self):
+    def _readChannel(self) -> list[str]:
         channels = []
         with open('./keys/channels.txt', 'r') as file:
             for line in file.readlines():
@@ -126,19 +126,19 @@ class Bot(commands.Bot):
         return channels
     # end _readChannel
 
-    async def _VerifyUser(self, user):
+    async def _verifyUser(self, user) -> twitchio.User | None:
         users = await self.fetch_users(names=[user])
         for i in users:
             if i.name.lower() == user.lower():
                 return i
         return None
-    # end _VerifyUser
+    # end _verifyUser
 
-    def start(self):
+    def start(self) -> None:
         self.run()
     # end start
 
-    async def event_ready(self):
+    async def event_ready(self) -> None:
         self._printLog(f'Logged in as | {self.nick}')
         self._printLog(f'User id is | {self.user_id}')
         for i in self.connected_channels:
@@ -151,46 +151,49 @@ class Bot(commands.Bot):
     #     print('Error: ', ctx.author.name, ': ', ctx.message.content, ' | ', error)
     # end event_command_error
 
-    async def event_message(self, ctx: commands.Context):
+    async def event_message(self, ctx: commands.Context) -> None:
         if type(ctx.author) != type(None):
-            user = await self._VerifyUser(ctx.author.name)
-            while not self.__agent._checkUserExistence(ctx.channel.name, user.id, user.name,):
-                self._printLog('Attempting to reconnect to the Database...')
-                if self.__agent.connect():
-                    self._printLog('Successfully reconnected to Database.')
-                else:
-                    self._printLog('Could not connect to Database.')
-                    self._printLog(f'Failed to add user {user.name} with id {user.id} to the database')
-            await self.handle_commands(ctx)
+            user = await self._verifyUser(ctx.author.name)
+            if user != None:
+                while not self.__agent._checkUserExistence(ctx.channel.name, user.id, user.name,):
+                    self._printLog('Attempting to reconnect to the Database...')
+                    if self.__agent.connect():
+                        self._printLog('Successfully reconnected to Database.')
+                    else:
+                        self._printLog('Could not connect to Database.')
+                        self._printLog(f'Failed to add user {user.name} with id {user.id} to the database')
+                await self.handle_commands(ctx)
     # end event_message
 
     ###         COMMANDS         ###
     @commands.command()
-    async def hello(self, ctx: commands.Context):
+    async def hello(self, ctx: commands.Context) -> None:
         await ctx.send(f'Hello there {ctx.author.name}!')
     # end hello
 
     @commands.command(name="howl")
-    async def howl_command(self, ctx: commands.Context):
+    async def howl_command(self, ctx: commands.Context) -> None:
         await ctx.send(f'Awooooooo!')
     # end howl_command
 
     @commands.command()
-    async def help(self, ctx: commands.Context):
+    async def help(self, ctx: commands.Context) -> None:
         await ctx.send(f'The following commands can be used: {', '.join(self.__command_names)}')
     # end help
 
     @commands.command()
-    async def uptime(self, ctx: commands.Context):
-        streamer = await self._VerifyUser(ctx.channel.name)
-        streams = await self.fetch_streams(user_ids=[streamer.id])
-        for i in streams:
-            if i.user.name == ctx.channel.name:
-                await ctx.send(f'{self.nick} has been live for {(datetime.datetime.now(datetime.UTC).replace(microsecond=0) - i.started_at.replace(microsecond=0))}')
+    async def uptime(self, ctx: commands.Context) -> None:
+        streamer = await self._verifyUser(ctx.channel.name)
+        if streamer != None:
+            streams = await self.fetch_streams(user_ids=[streamer.id])
+            for i in streams:
+                if i.user.name == ctx.channel.name:
+                    await ctx.send(f'{self.nick} has been live for {(datetime.datetime.now(datetime.UTC).replace(microsecond=0) - i.started_at.replace(microsecond=0))}')
+                    break
     # end uptime
 
     @commands.command(name="shoutout", aliases=("so",))
-    async def shoutout_command(self, ctx: commands.Context, targetUser:str):
+    async def shoutout_command(self, ctx: commands.Context, targetUser:str) -> None:
         # moderators = [] # Should see if the author is in a list of moderators
         if ctx.author.name == ctx.channel.name:
             exists = await self.fetch_channel(targetUser)#search_channel(user)
@@ -202,11 +205,11 @@ class Bot(commands.Bot):
     # end shoutout_command
 
     @commands.command(name="love")
-    async def love_command(self, ctx: commands.Context, targetUser:str | None):
+    async def love_command(self, ctx: commands.Context, targetUser:str | None) -> None:
         if targetUser != None:
-            user2 = await self._VerifyUser(targetUser)
+            user2 = await self._verifyUser(targetUser)
             if user2 != None:
-                user1 = await self._VerifyUser(ctx.author.name)
+                user1 = await self._verifyUser(ctx.author.name)
                 result1 = self.__agent.queryDB(ctx.channel.name, user1.id, user1.name, 'cookies')
                 result2 = self.__agent.queryDB(ctx.channel.name, user2.id, user2.name, 'cookies')
                 dice1 = self.__secretDiceBag[5].roll()*(result1['cookies']/100)*5
@@ -214,15 +217,16 @@ class Bot(commands.Bot):
                 result = (dice1+dice2) / 2
                 await ctx.send(f'The love between @{user1.name} and @{user2.name} is {result}%')
         else:
-            user1 = await self._VerifyUser(ctx.author.name)
-            result1 = self.__agent.queryDB(ctx.channel.name, user1.id, user1.name, 'cookies')
-            dice1 = self.__secretDiceBag[5].roll()*(result1['cookies']/100)*5
-            result = (dice1+self.__secretDiceBag[5].roll()*5)/2
-            await ctx.send(f'The love between @{user1.name} and WolfBot is {result}%')
+            user1 = await self._verifyUser(ctx.author.name)
+            if user1 != None:
+                result1 = self.__agent.queryDB(ctx.channel.name, user1.id, user1.name, 'cookies')
+                dice1 = self.__secretDiceBag[5].roll()*(result1['cookies']/100)*5
+                result = (dice1+self.__secretDiceBag[5].roll()*5)/2
+                await ctx.send(f'The love between @{user1.name} and WolfBot is {result}%')
     # end love_command
 
     @commands.command(name="who")
-    async def who_command(self, ctx: commands.Context):
+    async def who_command(self, ctx: commands.Context) -> None:
         people = []
         with open('who.txt', 'r') as file:
             for line in file.readlines():
@@ -239,25 +243,27 @@ class Bot(commands.Bot):
     @commands.command(name="cookie", aliases=("cookies", "biscuits"))
     async def cookie_command(self, ctx: commands.Context, targetUser:str | None) -> None:
         if targetUser == None:
-            user1 = await self._VerifyUser(ctx.author.name)
-            resultTime = self.__agent.queryDB(ctx.channel.name, user1.id, user1.name, 'cookiesRewardTime')
-            if datetime.datetime.now() > resultTime['cookiesRewardTime'] + datetime.timedelta(days=1):
-                amount = self.__secretDiceBag[5].roll()
-                self.__agent.incrementDB(ctx.channel.name, user1.id, user1.name, 'cookies', amount)
-                result = self.__agent.queryDB(ctx.channel.name, user1.id, user1.name, 'cookies')
-                if amount != 1:
-                    await ctx.send(f'@{user1.name} has been given {amount} cookies and they have {result['cookies']} cookies.')
+            user1 = await self._verifyUser(ctx.author.name)
+            if user1 != None:
+                resultTime = self.__agent.queryDB(ctx.channel.name, user1.id, user1.name, 'cookiesRewardTime')
+                if datetime.datetime.now() > resultTime['cookiesRewardTime'] + datetime.timedelta(days=1):
+                    amount = self.__secretDiceBag[5].roll()
+                    self.__agent.incrementDB(ctx.channel.name, user1.id, user1.name, 'cookies', amount)
+                    result = self.__agent.queryDB(ctx.channel.name, user1.id, user1.name, 'cookies')
+                    if amount != 1:
+                        await ctx.send(f'@{user1.name} has been given {amount} cookies and they have {result['cookies']} cookies.')
+                    else:
+                        await ctx.send(f'@{user1.name} has been given {amount} cookie and they have {result['cookies']} cookies.')
                 else:
-                    await ctx.send(f'@{user1.name} has been given {amount} cookie and they have {result['cookies']} cookies.')
-            else:
-                result = self.__agent.queryDB(ctx.channel.name, user1.id, user1.name, 'cookies')
-                await ctx.send(f'{user1.name} has {result['cookies']} cookies.')
+                    result = self.__agent.queryDB(ctx.channel.name, user1.id, user1.name, 'cookies')
+                    await ctx.send(f'{user1.name} has {result['cookies']} cookies.')
         else:
             if targetUser[0] == '@':
                 targetUser = targetUser[1:]
-            user2 = await self._VerifyUser(targetUser)
-            result = self.__agent.queryDB(ctx.channel.name, user2.id, user2.name, 'cookies')
-            await ctx.send(f'@{user2.name} has {result['cookies']} cookies.')
+            user2 = await self._verifyUser(targetUser)
+            if user2 != None:
+                result = self.__agent.queryDB(ctx.channel.name, user2.id, user2.name, 'cookies')
+                await ctx.send(f'@{user2.name} has {result['cookies']} cookies.')
     # end cookie_command
 
     @commands.command(name='give')
@@ -265,25 +271,26 @@ class Bot(commands.Bot):
         if targetUser != None:
             if targetUser[0] == '@':
                 targetUser = targetUser[1:]
-            user1 = await self._VerifyUser(ctx.author.name)
-            user2 = await self._VerifyUser(targetUser)
-            result = self.__agent.queryDB(ctx.channel.name, user1.id, user1.name, 'cookies')
-            if result['cookies'] == 0:
-                await ctx.send(f'{ctx.author.name} has no cookies to give.')
-            elif result['cookies'] > amount:
-                self.__agent.incrementDB(ctx.channel.name, user1.id, user1.name, 'cookies', -amount)
-                self.__agent.incrementDB(ctx.channel.name, user2.id, user2.name, 'cookies', amount)
-                if amount > 1:
-                    await ctx.send(f'{ctx.author.name} gave {amount} cookies to @{user2.name}.')
+            user1 = await self._verifyUser(ctx.author.name)
+            user2 = await self._verifyUser(targetUser)
+            if user1 != None and user2 != None:
+                result = self.__agent.queryDB(ctx.channel.name, user1.id, user1.name, 'cookies')
+                if result['cookies'] == 0:
+                    await ctx.send(f'{ctx.author.name} has no cookies to give.')
+                elif result['cookies'] > amount:
+                    self.__agent.incrementDB(ctx.channel.name, user1.id, user1.name, 'cookies', -amount)
+                    self.__agent.incrementDB(ctx.channel.name, user2.id, user2.name, 'cookies', amount)
+                    if amount > 1:
+                        await ctx.send(f'{ctx.author.name} gave {amount} cookies to @{user2.name}.')
+                    else:
+                        await ctx.send(f'{ctx.author.name} gave 1 cookie to @{user2.name}.')
                 else:
-                    await ctx.send(f'{ctx.author.name} gave 1 cookie to @{user2.name}.')
-            else:
-                self.__agent.incrementDB(ctx.channel.name, user1.id, user1.name, 'cookies', -result['cookies'])
-                self.__agent.incrementDB(ctx.channel.name, user2.id, user2.name, 'cookies', result['cookies'])
-                if result['cookies'] > 1:
-                    await ctx.send(f'{ctx.author.name} gave {result['cookies']} cookies to @{user2.name}.')
-                else:
-                    await ctx.send(f'{ctx.author.name} gave 1 cookie to @{user2.name}.')
+                    self.__agent.incrementDB(ctx.channel.name, user1.id, user1.name, 'cookies', -result['cookies'])
+                    self.__agent.incrementDB(ctx.channel.name, user2.id, user2.name, 'cookies', result['cookies'])
+                    if result['cookies'] > 1:
+                        await ctx.send(f'{ctx.author.name} gave {result['cookies']} cookies to @{user2.name}.')
+                    else:
+                        await ctx.send(f'{ctx.author.name} gave 1 cookie to @{user2.name}.')
     # end give_cookies
 
     @commands.command(name="steal")
@@ -291,41 +298,44 @@ class Bot(commands.Bot):
         if targetUser != None:
             if targetUser[0] == '@':
                 targetUser = targetUser[1:]
-            user1 = await self._VerifyUser(ctx.author.name)
-            user2 = await self._VerifyUser(targetUser)
-            result = self.__agent.queryDB(ctx.channel.name, user2.id, user2.name, 'cookies')
-            if result['cookies'] > 0:
-                # attack = self.roll(ctx, 'd20')
-                stealQuantity = random.randint(1, int(result['cookies']*0.1))
-                self.__agent.incrementDB(ctx.channel.name, user2.id, user2.name, 'cookies', -stealQuantity)
-                self.__agent.incrementDB(ctx.channel.name, user1.id, user1.name, 'cookies', stealQuantity)
-                result = self.__agent.queryDB(ctx.channel.name, user1.id, user1.name, 'cookies')
-                if stealQuantity == 1:
-                    await ctx.send(f'{ctx.author.name} stole {stealQuantity} cookie from @{user1.name}!')
+            user1 = await self._verifyUser(ctx.author.name)
+            user2 = await self._verifyUser(targetUser)
+            if user1 != None and user2 != None:
+                result = self.__agent.queryDB(ctx.channel.name, user2.id, user2.name, 'cookies')
+                if result['cookies'] > 0:
+                    # attack = self.roll(ctx, 'd20')
+                    stealQuantity = random.randint(1, int(result['cookies']*0.1))
+                    self.__agent.incrementDB(ctx.channel.name, user2.id, user2.name, 'cookies', -stealQuantity)
+                    self.__agent.incrementDB(ctx.channel.name, user1.id, user1.name, 'cookies', stealQuantity)
+                    result = self.__agent.queryDB(ctx.channel.name, user1.id, user1.name, 'cookies')
+                    if stealQuantity == 1:
+                        await ctx.send(f'{ctx.author.name} stole {stealQuantity} cookie from @{user1.name}!')
+                    else:
+                        await ctx.send(f'{ctx.author.name} stole {stealQuantity} cookies from @{user1.name}!')
                 else:
-                    await ctx.send(f'{ctx.author.name} stole {stealQuantity} cookies from @{user1.name}!')
-            else:
-                await ctx.send(f'@{user1.name} has no cookies to steal.')
+                    await ctx.send(f'@{user1.name} has no cookies to steal.')
     # end steal_cookies
 
     @commands.command(name='roll')
     async def roll(self, ctx:commands.Context, dice:str) -> None:
-        user = await self._VerifyUser(ctx.author.name)
-        for key, value in enumerate(['d4', 'd6', 'd8', 'd10', 'd12', 'd20']):
-            if dice == value:
-                roll = self.__diceBag[key].roll()
-                self.__agent.updateDB(ctx.channel.name, user.id, user.name, 'lastRoll', roll)
-                self.__agent.updateDB(ctx.channel.name, user.id, user.name, 'lastRolledDice', dice)
-                await ctx.send(f'{ctx.author.name} rolled a {dice} and got {roll}.')
-                break
+        user = await self._verifyUser(ctx.author.name)
+        if user != None:
+            for key, value in enumerate(['d4', 'd6', 'd8', 'd10', 'd12', 'd20']):
+                if dice == value:
+                    roll = self.__diceBag[key].roll()
+                    self.__agent.updateDB(ctx.channel.name, user.id, user.name, 'lastRoll', roll)
+                    self.__agent.updateDB(ctx.channel.name, user.id, user.name, 'lastRolledDice', dice)
+                    await ctx.send(f'{ctx.author.name} rolled a {dice} and got {roll}.')
+                    break
     # end roll
 
     @commands.command(name='lastRoll')
     async def lastRoll(self, ctx:commands.Context) -> None:
-        user = await self._VerifyUser(ctx.author.name)
-        roll = self.__agent.queryDB(ctx.channel.name, user.id, user.name, 'lastRoll')
-        dice = self.__agent.queryDB(ctx.channel.name, user.id, user.name, 'lastRolledDice')
-        await ctx.send(f'{ctx.author.name} rolled a {dice['lastRolledDice']} and got {roll['lastRoll']}.')
+        user = await self._verifyUser(ctx.author.name)
+        if user != None:
+            roll = self.__agent.queryDB(ctx.channel.name, user.id, user.name, 'lastRoll')
+            dice = self.__agent.queryDB(ctx.channel.name, user.id, user.name, 'lastRolledDice')
+            await ctx.send(f'{ctx.author.name} rolled a {dice['lastRolledDice']} and got {roll['lastRoll']}.')
     # end lastRoll
 
     @commands.command(name='rollHistory')
